4fe04e521d8ad66bc610a0a24bedb90b6c8a6cad
sjsjsj1246 <sjsjsj1246@gmail.com>
2022-07-15 19:09:09 +0900

    Feat: FutureBuilder

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -14,33 +14,58 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     final textStyle = TextStyle(fontSize: 16.0);
 
-    return Scaffold(body: FutureBuilder(builder: (context, snapshot) {
-      return Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text("futerBuilder",
-                style: textStyle.copyWith(
-                    fontWeight: FontWeight.w700, fontSize: 20.0)),
-            Text("ConState : ${snapshot.connectionState}",
-                style: textStyle.copyWith(
-                    fontWeight: FontWeight.w700, fontSize: 20.0)),
-            Text("Data : ${snapshot.data}",
-                style: textStyle.copyWith(
-                    fontWeight: FontWeight.w700, fontSize: 20.0)),
-            Text("Error : ${snapshot.error}",
-                style: textStyle.copyWith(
-                    fontWeight: FontWeight.w700, fontSize: 20.0)),
-          ],
-        ),
-      );
-    }));
+    return Scaffold(
+        body: FutureBuilder(
+            future: getNumber(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      if (snapshot.connectionState ==
+                          ConnectionState.waiting) ...[
+                        SizedBox(width: 10.0),
+                        CircularProgressIndicator()
+                      ],
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Text("futureBuilder",
+                              style: textStyle.copyWith(
+                                  fontWeight: FontWeight.w700, fontSize: 20.0)),
+                          Text("ConState : ${snapshot.connectionState}",
+                              style: textStyle),
+                          Text("Data : ${snapshot.data}", style: textStyle),
+                          Text("Error : ${snapshot.error}", style: textStyle),
+                          ElevatedButton(
+                              onPressed: () {
+                                setState(() {});
+                              },
+                              child: Text("Refresh")),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              }
+
+              if (snapshot.hasError) {
+                return Center(
+                  child: Icon(Icons.error),
+                );
+              }
+
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }));
   }
 
   Future<int> getNumber() async {
-    await Future.delayed(const Duration(seconds: 3));
+    await Future.delayed(const Duration(seconds: 2));
 
     final random = Random();
 
